dd805caaebc437300f98c83d3dad21963cbd1bf8
TaylorNAlbarnaz <veloxgods@gmail.com>
2023-02-08 18:27:35 -0300

    First class about methods

--- src/edu/taylor/modulo1/Main.java
+++ src/edu/taylor/modulo1/Main.java
@@ -1,7 +1,16 @@
 package edu.taylor.modulo1;
 
+
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Hello world!");
+        int primeiroValor = 4;
+        int segundoValor = 6;
+
+        var resultado = multiplicar(primeiroValor, segundoValor);
+        System.out.print(resultado);
+    }
+    
+    public static int multiplicar(int primeiroValor, int segundoValor) {
+        return primeiroValor * segundoValor;
     }
 }
