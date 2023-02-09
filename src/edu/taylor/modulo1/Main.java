a5afe5a2f848e588f0e4503203314d62f12a05ac
TaylorNAlbarnaz <veloxgods@gmail.com>
2023-02-08 22:59:39 -0300

    Refactor for non-static class

--- src/edu/taylor/modulo1/Main.java
+++ src/edu/taylor/modulo1/Main.java
@@ -3,14 +3,7 @@ package edu.taylor.modulo1;
 
 public class Main {
     public static void main(String[] args) {
-        int primeiroValor = 4;
-        int segundoValor = 6;
-
-        var resultado = multiplicar(primeiroValor, segundoValor);
-        System.out.print(resultado);
-    }
-    
-    public static int multiplicar(int primeiroValor, int segundoValor) {
-        return primeiroValor * segundoValor;
+        Tipos tipos = new Tipos();
+        tipos.mostrarVariaveis();
     }
 }
--- src/edu/taylor/modulo1/Tipos.java
+++ src/edu/taylor/modulo1/Tipos.java
@@ -1,20 +1,16 @@
 package edu.taylor.modulo1;
 
 public class Tipos {
-    public static void main(String[] args) {
-        mostrarVariaveis();
-    }
+    byte idade = 21;
+    short ano = 2023;
+    int cep = 96570000;
+    char letra = 'T';
+    String nome = "Taylor";
+    Long numGalaxias = 2000000000000L;
+    float numPequeno = 50000.00f;
+    double numGrande = 100000000000.00;
 
-    public static void mostrarVariaveis() {
-        byte idade = 21;
-        short ano = 2023;
-        int cep = 96570000;
-        char letra = 'T';
-        String nome = "Taylor";
-        Long numGalaxias = 2000000000000L;
-        float numPequeno = 50000.00f;
-        double numGrande = 100000000000.00;
-
+    public void mostrarVariaveis() {
         System.out.print(idade + " " + ano + " " + cep + " " + letra + " " + " " + nome +
                 " " + numGalaxias + " " + numPequeno + " " + numGrande);
     }
